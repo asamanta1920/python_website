15f4f0144860c9803f6ab22da1e87895f401a782
Tumpa Sarkar <58119007+mummum2019@users.noreply.github.com>
2019-11-23 21:28:49 -0500

    Say hello with persons name if given

--- app1.py
+++ app1.py
@@ -2,10 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello')
 @app.route('/<username>')
-
 def hello_world(username = None):
-    name = username
     print(username)
-    return render_template('hello_is_none.html') if not name else render_template('hello1.html', name)
+    return render_template('hello.html', name = username)
